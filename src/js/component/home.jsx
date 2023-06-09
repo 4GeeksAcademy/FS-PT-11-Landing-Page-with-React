0f7671a11076324d48d416a2412f2ec565765cc2
Xomano <129993903+Xomano@users.noreply.github.com>
2023-06-09 21:55:12 +0100

    Creating Cards on document

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "./navbar.jsx";
 import { Jumbotron } from "./jumbotron.jsx";
+import { Card } from "./card.jsx";
 
 //create your first component
 const Home = () => {
@@ -11,6 +12,9 @@ const Home = () => {
       </div>
       <div className="container-fluid  m-0 p-0">
         <Jumbotron />
+      </div>
+      <div className="container-fluid  m-0 p-0">
+        <Card />
       </div>
     </div>
   );
