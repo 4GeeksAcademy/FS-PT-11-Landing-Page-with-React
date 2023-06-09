f70cff8e26f30555295f1b07558539f10dbc0277
Adriano Schneider Maximiano <129993903+Xomano@users.noreply.github.com>
2023-06-09 23:03:58 +0000

    Adding the card.jsx file to document

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,20 +1,89 @@
-import react from "react";
+import React from "react";
 
 export const Card = () => {
   return (
-    <div className="container-fluid bg-light">
-      <div className="container pt-5 pb-5">
-        <h1 className="display-1">A Warm Welcome</h1>
-        <p className="mt-3 mb-3">
-          The key of this project is to help you understand how to pick the
-          components that you want to build, you have to think the best way to
-          organize and avoid future errors. What can be re-used? If your code is
-          tuning into something big and hard to understand you probably will
-          need to split it into components:
-        </p>
-        <a href="#" className="btn btn-primary">
-          Call to action
-        </a>
+    <div className="container">
+      <div className="row d-flex flex-lg-row flex-sm-column justify-content-center">
+        <div
+          className="card text-center m-3 p-0 col-sm-10 col-lg-2"
+          style={{ width: "17rem" }}
+        >
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Find out more
+            </a>
+          </div>
+        </div>
+        <div
+          className="card text-center m-3 p-0 col-sm-10 col-lg-2"
+          style={{ width: "17rem" }}
+        >
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Find out more
+            </a>
+          </div>
+        </div>
+        <div
+          className="card text-center m-3 p-0 col-sm-10 col-lg-2"
+          style={{ width: "17rem" }}
+        >
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Find out more
+            </a>
+          </div>
+        </div>
+        <div
+          className="card text-center m-3 p-0 col-sm-10 col-lg-2"
+          style={{ width: "17rem" }}
+        >
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Find out more
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
