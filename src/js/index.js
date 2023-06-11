2ed152f515e324523548fd614fb4f4a02d0d32b0
Xomano <129993903+Xomano@users.noreply.github.com>
2023-06-11 20:46:40 +0100

    Final toutch on color, changing title name and removing unnecessary comment from index.js

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,6 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-// import Navbar from "./component/navbar";
 
 //render your react application
-// ReactDOM.render(<Navbar />, document.querySelector("#app"))
 ReactDOM.render(<Home />, document.querySelector("#app"));
